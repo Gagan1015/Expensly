3cb3211da68dcb40c498ea50b3a7038f008b64e1
Gagan1015 <gagansaini1510@gmail.com>
2025-11-15 06:28:53 +0530

    Add debug info for view loading

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -11,6 +11,19 @@ class LandingController extends Controller
      */
     public function index()
     {
+        // Debug: Check if view file exists
+        $viewPath = resource_path('views/landing.blade.php');
+        
+        if (!file_exists($viewPath)) {
+            return response()->json([
+                'error' => 'View file not found',
+                'path' => $viewPath,
+                'resource_path' => resource_path(),
+                'views_dir_exists' => is_dir(resource_path('views')),
+                'views_contents' => is_dir(resource_path('views')) ? scandir(resource_path('views')) : 'Directory not found'
+            ], 500);
+        }
+        
         return view('landing');
     }
 }
