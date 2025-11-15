957fa9623882968236472cd2ebbeb8271712a996
Gagan1015 <gagansaini1510@gmail.com>
2025-11-15 06:30:17 +0530

    Return diagnostic JSON instead of view

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -11,19 +11,18 @@ class LandingController extends Controller
      */
     public function index()
     {
-        // Debug: Check if view file exists
-        $viewPath = resource_path('views/landing.blade.php');
-        
-        if (!file_exists($viewPath)) {
-            return response()->json([
-                'error' => 'View file not found',
-                'path' => $viewPath,
-                'resource_path' => resource_path(),
-                'views_dir_exists' => is_dir(resource_path('views')),
-                'views_contents' => is_dir(resource_path('views')) ? scandir(resource_path('views')) : 'Directory not found'
-            ], 500);
-        }
-        
-        return view('landing');
+        // Debug: Return all diagnostic information
+        return response()->json([
+            'resource_path' => resource_path(),
+            'views_path' => resource_path('views'),
+            'landing_file_path' => resource_path('views/landing.blade.php'),
+            'file_exists' => file_exists(resource_path('views/landing.blade.php')),
+            'views_dir_exists' => is_dir(resource_path('views')),
+            'views_readable' => is_readable(resource_path('views')),
+            'views_contents' => is_dir(resource_path('views')) ? array_slice(scandir(resource_path('views')), 0, 20) : 'Directory not found',
+            'view_paths' => config('view.paths', []),
+            'app_path' => app_path(),
+            'base_path' => base_path(),
+        ]);
     }
 }
