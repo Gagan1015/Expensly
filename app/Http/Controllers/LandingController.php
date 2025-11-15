334fb771ff6107571b015e1d47f76066873adfc5
Gagan1015 <gagansaini1510@gmail.com>
2025-11-15 06:31:46 +0530

    Fix Heroku cache issue - clear config after install

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -11,18 +11,6 @@ class LandingController extends Controller
      */
     public function index()
     {
-        // Debug: Return all diagnostic information
-        return response()->json([
-            'resource_path' => resource_path(),
-            'views_path' => resource_path('views'),
-            'landing_file_path' => resource_path('views/landing.blade.php'),
-            'file_exists' => file_exists(resource_path('views/landing.blade.php')),
-            'views_dir_exists' => is_dir(resource_path('views')),
-            'views_readable' => is_readable(resource_path('views')),
-            'views_contents' => is_dir(resource_path('views')) ? array_slice(scandir(resource_path('views')), 0, 20) : 'Directory not found',
-            'view_paths' => config('view.paths', []),
-            'app_path' => app_path(),
-            'base_path' => base_path(),
-        ]);
+        return view('landing');
     }
 }
